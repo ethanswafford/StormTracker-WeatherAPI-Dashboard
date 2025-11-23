a5a60c5dc2c0d121e96fff6c784eaa1c4727107a
Ethan Swafford <ethanswafford@yahoo.com>
2025-11-23 15:37:00 -0500

    grabDomEl

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,8 @@
 console.log('JS Connected');
+
+// Grab DOM Elements
+const searchForm = document.getElementById("search-form");
+const location = document.getElementById("location-input");
+const historyEl = document.getElementById("history");
+const currentWeather = document.getElementById("current-weather");
+const forecastEl = document.getElementById("forecast");
