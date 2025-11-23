97ac922f53b48255819e62068932b4f20c252177
Ethan Swafford <ethanswafford@yahoo.com>
2025-11-23 15:43:55 -0500

    prevDefAndEventListener

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,17 @@ console.log('JS Connected');
 
 // Grab DOM Elements
 const searchForm = document.getElementById("search-form");
-const location = document.getElementById("location-input");
+const locationEl = document.getElementById("location-input");
 const historyEl = document.getElementById("history");
 const currentWeather = document.getElementById("current-weather");
 const forecastEl = document.getElementById("forecast");
+
+// eventlistener and prevent default
+searchForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const location = locationEl.ariaValueMax.trim();
+    if (!location) return;
+    fetchAndRenderWeather(location);
+    addLocationToHistory(location);
+    locationEl.value = "";
+})
